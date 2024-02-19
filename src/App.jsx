bfafdae6ed133db06f205d01382246ead0355ef7
taphuonglinh <taphuonglinh@luvina.net>
2024-02-19 16:45:47 +0700

    save get localstorage ÃuseEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useRef, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 
 import Places from './components/Places.jsx';
 import {AVAILABLE_PLACES} from './data.js';
@@ -7,20 +7,35 @@ import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import {sortPlacesByDistance} from "./loc.js";
 
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    const storedPlaces =storedIds.map((id) => AVAILABLE_PLACES.find((place) => place.id === id));
 function App() {
     const modal = useRef();
     const selectedPlace = useRef();
-    const [pickedPlaces, setPickedPlaces] = useState([]);
+    const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
+    const [availablePlaces, setAvailablePlaces] = useState([]);
 
-    // lấy vị trí hiện tại thiết bị người dùng
-    // arrow function bên trong sẽ được thực thi ngay sau khi lấy được vị trí
-    navigator.geolocation.getCurrentPosition((position) => {
-        const sortedPlaces = sortPlacesByDistance(
-            AVAILABLE_PLACES,
-            position.coords.latitude,
-            position.coords.longitude
-        )
-    });
+    // useEffect(() => {
+    //     const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    //     const storedPlaces =storedIds.map((id) => AVAILABLE_PLACES.find((place) => place.id === id));
+    //     setPickedPlaces(storedPlaces);
+    // }, []);
+
+    useEffect(() => {
+        // lấy vị trí hiện tại thiết bị người dùng
+        // arrow function bên trong sẽ được thực thi ngay sau khi lấy được vị trí
+        navigator.geolocation.getCurrentPosition((position) => {
+            const sortedPlaces = sortPlacesByDistance(
+                AVAILABLE_PLACES,
+                position.coords.latitude,
+                position.coords.longitude
+            );
+
+            setAvailablePlaces(sortedPlaces);
+        });
+
+    }, []);
+
 
     function handleStartRemovePlace(id) {
         modal.current.open();
@@ -39,6 +54,12 @@ function App() {
             const place = AVAILABLE_PLACES.find((place) => place.id === id);
             return [place, ...prevPickedPlaces];
         });
+        const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+        if (storedIds.indexOf(id) === -1) {
+            // lưu vào localstorage
+            localStorage.setItem('selectedPlaces', JSON.stringify([id, ...storedIds]));
+        }
+
     }
 
     function handleRemovePlace() {
@@ -46,6 +67,10 @@ function App() {
             prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
         );
         modal.current.close();
+
+        const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+        // lưu vào localstorage
+        localStorage.setItem('selectedPlaces', JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current)));
     }
 
     return (
@@ -74,7 +99,8 @@ function App() {
                 />
                 <Places
                     title="Available Places"
-                    places={AVAILABLE_PLACES}
+                    places={availablePlaces}
+                    fallbackText="Sorting places by distance..."
                     onSelectPlace={handleSelectPlace}
                 />
             </main>
